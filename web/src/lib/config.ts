1946cf42c58fb9a634f16dcb29e00085de3abcb4
junhyuk0801 <junhyuk0801@gmail.com>
2023-11-15 17:08:25 +0900

    remove: seperate web code and backend

--- web/src/lib/config.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-	apiHost: 'https://jhseodev1.site:8443',
-	apiWsHost: 'wss://jhseodev1.site:8443',
-};
--- web/src/routes/tetrisgame/+page.ts
+++
@@ -1,6 +0,0 @@
-import page from './index.html?raw';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async () => {
-	return { page };
-};
